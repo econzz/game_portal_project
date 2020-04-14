fea56613e0c73b416f9c2cf3e1da9d2fdef541a9
Nelson Widjaya <nelson.widjaya@gmail.com>
2020-04-15 01:04:50 +0900

    #6 implement upload zip -> unzip, to public folder, but its not working, so commit first

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,12 +10,24 @@ class GamesController < ApplicationController
 
   def create
     #render plain: params[:game].inspect
-
+    puts "================= START CREATE= "
     @game = Game.new(game_params)
     @game.cover_big = rails_blob_path(@game.cover_image, disposition: "attachment")
-    @game.path = rails_blob_path(@game.game_zip, disposition: "attachment")
+    
+       
+      Zip::ZipFile.open(game_params[:game_zip].tempfile) { |zip_file|
+        zip_file.each { |f|
+          f_path=Rails.root.join('public/'+game_params[:game_zip].original_filename, f.name)
+          puts "================= = " + f_path.to_s
+          FileUtils.mkdir_p(File.dirname(f_path))
+          zip_file.extract(f, f_path) unless File.exist?(f_path)
+        }
+      }
+      
+  
     
     if @game.save
+      puts "================= = END"
       #unzip
       flash[:notice] = "Game is successfully saved"
       redirect_to games_path(@game)
@@ -25,28 +37,28 @@ class GamesController < ApplicationController
     
   end
 
-  def unzip
-    zipfile = params[:zip_file]
-      Zip::File.open(rails_blob_path(@game.game_zip, disposition: "attachment")) do |zip|
-        zip.each do |entry|
-          ext = File.extname(entry.name)
-          # 隠しファイルやゴミファイルは無視
-          next if ext.blank? || File.basename(entry.name).count(".") > 1
-          # テンポラリファイルを開く
-          Tempfile.open([File.basename(entry.to_s), ext]) do |file|
-            begin
-              # 一時ファイルを作成
-              entry.extract(file.path) { true }
-              # モデルを作成
-              image = Image.create!(source: file)
-            ensure
-              # 終わったらclose
-              file.close!
-          end
-        end
-      end
-    end
-  end
+  # def unzip
+  #   zipfile = params[:zip_file]
+  #     Zip::File.open(rails_blob_path(@game.game_zip, disposition: "attachment")) do |zip|
+  #       zip.each do |entry|
+  #         ext = File.extname(entry.name)
+  #         # 隠しファイルやゴミファイルは無視
+  #         next if ext.blank? || File.basename(entry.name).count(".") > 1
+  #         # テンポラリファイルを開く
+  #         Tempfile.open([File.basename(entry.to_s), ext]) do |file|
+  #           begin
+  #             # 一時ファイルを作成
+  #             entry.extract(file.path) { true }
+  #             # モデルを作成
+  #             image = Image.create!(source: file)
+  #           ensure
+  #             # 終わったらclose
+  #             file.close!
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 
   ####
   def edit
